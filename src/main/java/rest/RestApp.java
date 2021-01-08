2682d65ad15152ecea9b4bb71192d7b75bebb7ff
OlegTselishchev <oleks63@mail.ru>
2021-01-09 01:43:35 +0400

    add coment

--- src/main/java/rest/RestApp.java
+++ src/main/java/rest/RestApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApp {
+    //start
     public static void main(String[] args) {
         SpringApplication.run(RestApp.class, args);
     }
